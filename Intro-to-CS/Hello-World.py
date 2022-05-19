10f55615ed15ca4c9ccdf9f7a03c2b40747c138a
Weston Gibson <89497650+tydianin@users.noreply.github.com>
2022-05-19 15:36:21 -0700

    Create Hello-World.py
    One assignment at a time.

---
+++ Intro-to-CS/Hello-World.py
@@ -0,0 +1,7 @@
+# Assignment 1
+# Print Information
+# Written by: Weston Gibson
+
+print('Name: Weston Gibson')
+print('Course: CISP 300 LEC 10455 - Algorithm Design and Problem Solving')
+print('Existential Quote: The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.')
