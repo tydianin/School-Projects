96100ffc498cba8c573b8c072f1a8943d1ab29c4
Weston Gibson <89497650+tydianin@users.noreply.github.com>
2022-05-20 07:25:05 -0700

    Create 01_Hello.cpp

---
+++ Intro to Structured Programming (C++)/01_Hello.cpp
@@ -0,0 +1,15 @@
+// Assignment: Demo1
+// Programmer: Weston Gibson
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string userName;
+
+	cout << "Enter your name: ";
+	cin >> userName;
+
+	cout << "\nHello " << userName;
+}
